15831b772d813559f6c72fd846eb94a2cf7a4542
NGigengack <info@niekgigengack.nl>
2020-06-10 12:26:04 +0200

    Tried to work on the dedup logic but the django migrations don't seem to be doing what I want them to do. Also cleaned up some comments from files.

--- consumer/worker/tasks.py
+++ consumer/worker/tasks.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
-from celery.contrib import rdb
 from celery.utils.log import get_task_logger
 from consumer.celery import app
 logger = get_task_logger(__name__)
@@ -8,9 +7,6 @@ logger = get_task_logger(__name__)
 
 @shared_task
 def filter_task(message):
-    # result = SyncBag.objects.filter(id=message['id'])
-    # print(result.count())
-    # rdb.set_trace()
     logger.error('========> received message: {0!r}'.format(message))
     if message['syncItem']['grade'] < 'C':
         send_dedup_message(message)
